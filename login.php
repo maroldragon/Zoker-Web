b2dcd62818db44316def0a7b1399129dd8ff1d71
Iman Fajar Lombu <vhajar18@gmail.com>
2021-06-04 20:28:45 +0700

    Push Pertama

---
+++ login.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="navbar d-flex bgcolorcyan">
+    <div class="container">
+      <a href="#" class="navbar-brand"><img src="img/logo.png" width="130" height="40" class="d-inline-block align-top" alt=""></a>
+      <input type="text" class="search" placeholder="Cari ...">
+      <div class="ms-auto">
+        <ul class="navbar nav">
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">KATEGORI</a></li>
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">KONTAK</a></li>
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">TENTANG</a></li>
+          <li class="nav-item active"><a class="nav-link ftcolordark" href="#"><i class="fas fa-bell"></i></a></li>
+          <li class="nav-item active"><a class="nav-link ftcolordark" href="#"><i class="fas fa-user-circle fa-2x"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5 ps-3 pe-3">
+    <div class="loginform">
+      <div class="row">
+        <div class="col-md-6 bgcolorcyan p-5">
+          <img src="img/login.svg" class="w-100" alt="">
+        </div>
+        <div class="col-md-6 p-5">
+          <h5>Welcome Back</h5>
+          <p>Username</p>
+          <input type="text" class="" placeholder="Penulis">
+          <p>Password</p>
+          <input type="password" class="" placeholder="Penulis">
+          <br>
+          <button>LOGIN</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="bgcolorcyan footer mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">PERPUSTAKAAN DIGITAL</h5>
+          <p>Aplikasi Rekomendasi Perpustakaan Digital Menggunakan Deep Collaborative Filtering Berbasis Mobile Dan Web</p>
+          <p>Copyright &#169; 2021 Perpustakaan Digital | Zoker</p>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5>MENU</h5>
+          <ul class="list-unstyled">
+            <li><a class="nav-link ftcolordark" href="#">Home</a></li>
+            <li><a class="nav-link ftcolordark" href="#">Koleksi</a></li>
+            <li><a class="nav-link ftcolordark" href="#">Profil</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">FOLLOW US</h5>
+          <a class="ftcolordark" href="https://www.facebook.com"><i class="fab fa-facebook fa-2x"></i></a>
+          <a class="ftcolordark" href="https://www.instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
+          <a class="ftcolordark" href="https://www.twitter.com"><i class="fab fa-twitter fa-2x"></i></a>          
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Evan Owen Pasaribu</h5>
+          <a class="ftcolordark" href="#">evanowenpasaribu@gmail.com</p></a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Iman Fajar Lombu</h5>
+          <a class="ftcolordark" href="#">imanfajarlombu@gmail.com</p></a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Martua Sinaga</h5>
+          <a class="ftcolordark" href="#">martuansinaga@gmail.com</p></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="navbar d-flex bgcolorcyan">
+    <div class="container">
+      <a href="#" class="navbar-brand"><img src="img/logo.png" width="130" height="40" class="d-inline-block align-top" alt=""></a>
+      <input type="text" class="search" placeholder="Cari ...">
+      <div class="ms-auto">
+        <ul class="navbar nav">
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">KATEGORI</a></li>
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">KONTAK</a></li>
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">TENTANG</a></li>
+          <li class="nav-item active"><a class="nav-link ftcolordark" href="#"><i class="fas fa-bell"></i></a></li>
+          <li class="nav-item active"><a class="nav-link ftcolordark" href="#"><i class="fas fa-user-circle fa-2x"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5 ps-3 pe-3">
+    <div class="row">
+      <div class="col-md-4">
+        <p>Judul Buku</p>
+        <input type="text" class="" placeholder="Judul Buku">
+      </div>
+      <div class="col-md-4">
+        <p>Penulis</p>
+        <input type="text" class="" placeholder="Penulis">
+      </div>
+      <div class="col-md-4">
+        <p>ISBN</p>
+        <input type="text" class="" placeholder="ISBN">
+      </div>
+    </div>
+    <button class="mt-3">CARI</button>
+    <h3 class="mt-5">Hasil Pencarian</h3>
+    <div class="row">
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top rounded-0" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="bgcolorcyan footer mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">PERPUSTAKAAN DIGITAL</h5>
+          <p>Aplikasi Rekomendasi Perpustakaan Digital Menggunakan Deep Collaborative Filtering Berbasis Mobile Dan Web</p>
+          <p>Copyright &#169; 2021 Perpustakaan Digital | Zoker</p>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5>MENU</h5>
+          <ul class="list-unstyled">
+            <li><a class="nav-link ftcolordark" href="#">Home</a></li>
+            <li><a class="nav-link ftcolordark" href="#">Koleksi</a></li>
+            <li><a class="nav-link ftcolordark" href="#">Profil</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">FOLLOW US</h5>
+          <a class="ftcolordark" href="https://www.facebook.com"><i class="fab fa-facebook fa-2x"></i></a>
+          <a class="ftcolordark" href="https://www.instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
+          <a class="ftcolordark" href="https://www.twitter.com"><i class="fab fa-twitter fa-2x"></i></a>          
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Evan Owen Pasaribu</h5>
+          <a class="ftcolordark" href="#">evanowenpasaribu@gmail.com</p></a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Iman Fajar Lombu</h5>
+          <a class="ftcolordark" href="#">imanfajarlombu@gmail.com</p></a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Martua Sinaga</h5>
+          <a class="ftcolordark" href="#">martuansinaga@gmail.com</p></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
