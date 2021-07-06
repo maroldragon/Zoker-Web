e40d31a74a0bfdc49d2e088c56ff2fac5b6efce1
Evan Owen Pasaribu <evanowenpasaribu7@gmail.com>
2021-07-06 17:22:39 +0700

    Make Simple Edit

---
+++ header.php
@@ -0,0 +1,27 @@
+<div class="navbar navbar-expand-lg d-flex bgcolorcyan">
+	<div class="container">
+		<a href="index.php" class="navbar-brand"><img src="img/logo.png" width="130" height="40" class="d-inline-block align-top" alt=""></a>
+		<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+  	</button> -->
+		<div class="" id="navbarSupportedContent">
+			<ul class="navbar nav ms-auto">
+				<input type="text" class="search" placeholder="Cari Buku ...">
+				<li class="nav-item"><a class="nav-link ftcolordark" href="kategori.php">KATEGORI</a></li>
+				<li class="nav-item"><a class="nav-link ftcolordark" href="#">KONTAK</a></li>
+				<li class="nav-item"><a class="nav-link ftcolordark" href="#">TENTANG</a></li>
+				<li class="nav-item active"><a class="nav-link ftcolordark notifikasi" href="#"><i class="#"></i></a></li>
+				<li class="nav-item active"><a class="nav-link ftcolordark user" href="#"><i class="#"></i></a></li>
+				<div class="dropdown-menu dropdown-menu-end">
+					<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+					<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="pages-settings.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
+					<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Log out</a>
+				</div>
+			</ul>
+		</div>
+	</div>
+</div>
