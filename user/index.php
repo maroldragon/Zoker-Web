f8df796ab8152ef847783429d9a507aa25a1ea3b
maroldragon <autobot2690@gmail.com>
2021-07-07 22:45:15 +0700

    revisi 3.5

---
+++ user/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+  <title>Index</title>
+</head>
+<body>
+  <div class="navbar d-flex bgcolorcyan">
+    <div class="container">
+      <a href="index.php" class="navbar-brand"><img src="img/logo.png" width="130" height="40" class="d-inline-block align-top" alt=""></a>
+      <input type="text" class="search" placeholder="Cari Buku ...">
+      <div class="ms-auto">
+        <ul class="navbar nav">
+          <li class="nav-item"><a class="nav-link ftcolordark" href="kategori.php">KATEGORI</a></li>
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">KONTAK</a></li>
+          <li class="nav-item"><a class="nav-link ftcolordark" href="#">TENTANG</a></li>
+          <li class="nav-item active"><a class="nav-link ftcolordark notifikasi" href="#"><i class="#"></i></a></li>
+          <li class="nav-item active"><a class="nav-link ftcolordark user" href="#"><i class="#"></i></a></li>
+          <div class="dropdown-menu dropdown-menu-end">
+            <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="pages-settings.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
+            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Log out</a>
+          </div>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5 ps-3 pe-3">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/slider.png" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/slider.png" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/slider.png" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="mt-5">Baru Ditambahkan</h3>
+    <div class="row">
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top rounded-0" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+        <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+        <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+        <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+    </div>
+    <h3 class="mt-5">Rekomendasi</h3>
+    <div class="row">
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+          <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+          <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+          <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+          <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+          <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+          <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+          <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+          <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+          <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+          <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+          <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+          <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+          <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+          <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+          <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+      <div class="col-md-2 col-sm-4 col-6 card p-2 border-0">
+          <div class="position-absolute text-warning bg-dark p-1"><i class="fas fa-star"><p class="d-inline text-light ps-2">4,5</p></i></div>
+          <img src="img/coverbook.jpg" class="card-img-top" alt="...">
+          <div class="card-body"><h6 class="card-title">Under the Black Flag: The Romance and the Reality of Life Among the Pirates</h5><p class="card-text"><i>David Cordingly</i></p></div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="bgcolorcyan footer mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">PERPUSTAKAAN DIGITAL</h5>
+          <p>Aplikasi Rekomendasi Perpustakaan Digital Menggunakan Deep Collaborative Filtering Berbasis Mobile Dan Web</p>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5>MENU</h5>
+          <ul class="list-unstyled">
+            <li><a class="nav-link ftcolordark" href="#">Home</a></li>
+            <li><a class="nav-link ftcolordark" href="#">Koleksi</a></li>
+            <li><a class="nav-link ftcolordark" href="#">Profil</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">FOLLOW US</h5>
+          <a class="ftcolordark" href="https://www.facebook.com"><i class="fab fa-facebook fa-2x"></i></a>
+          <a class="ftcolordark" href="https://www.instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
+          <a class="ftcolordark" href="https://www.twitter.com"><i class="fab fa-twitter fa-2x"></i></a>          
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Evan Owen Pasaribu</h5>
+          <a class="ftcolordark" href="#">evanowenpasaribu@gmail.com</p></a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Iman Fajar Lombu</h5>
+          <a class="ftcolordark" href="#">imanfajarlombu@gmail.com</p></a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <h5 class="warna">Martua Sinaga</h5>
+          <a class="ftcolordark" href="#">martuansinaga@gmail.com</p></a>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <p>Copyright &#169; 2021 Perpustakaan Digital | Zoker</p>
+      </div>
+    </div>
+  </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
