f8921d424e65e16c50a9a28fcb48193d40750358
Iman Fajar Lombu <vhajar18@gmail.com>
2021-07-09 11:03:28 +0700

    Push Rank

--- user/footer.php
+++ user/footer.php
@@ -1,39 +1,40 @@
-<footer class="bgcolorcyan footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 col-sm-6 col-12">
-        <h5 class="warna">PERPUSTAKAAN DIGITAL</h5>
-        <p>Aplikasi Rekomendasi Perpustakaan Digital Menggunakan Deep Collaborative Filtering Berbasis Mobile Dan Web</p>
-        <p>Copyright &#169; 2021 Perpustakaan Digital | Zoker</p>
+<footer>
+  <div class="container-lg position-relative">
+    <div class="row mb-2">
+      <div class="col pos-left">
+        <h1>Navigasi</h1>
       </div>
-      <div class="col-md-4 col-sm-6 col-12">
-        <h5>MENU</h5>
-        <ul class="list-unstyled">
-          <li><a class="nav-link ftcolordark" href="#">Home</a></li>
-          <li><a class="nav-link ftcolordark" href="#">Koleksi</a></li>
-          <li><a class="nav-link ftcolordark" href="#">Profil</a></li>
-        </ul>
-      </div>
-      <div class="col-md-4 col-sm-6 col-12">
-        <h5 class="warna">FOLLOW US</h5>
-        <a class="ftcolordark" href="https://www.facebook.com"><i class="fab fa-facebook fa-2x"></i></a>
-        <a class="ftcolordark" href="https://www.instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
-        <a class="ftcolordark" href="https://www.twitter.com"><i class="fab fa-twitter fa-2x"></i></a>          
+      <div class="col pos-right">
+        <h1>Perpustakaan Digital</h1>
       </div>
     </div>
-    <div class="row mt-3">
-      <div class="col-md-4 col-sm-6 col-12">
-        <h5 class="warna">Evan Owen Pasaribu</h5>
-        <a class="ftcolordark" href="#">evanowenpasaribu@gmail.com</p></a>
+    <div class="row">
+      <div class="col pos-left">
+        <ul class="list-unstyled">
+          <li><a class="d-flex footer-link" href="#">Beranda</a></li>
+          <li><a class="d-flex footer-link" href="#">Koleksi</a></li>
+          <li><a class="d-flex footer-link" href="#">Kontak</a></li>
+          <li><a class="d-flex footer-link" href="#">Tentang Kami</a></li>
+        </ul>
       </div>
-      <div class="col-md-4 col-sm-6 col-12">
-        <h5 class="warna">Iman Fajar Lombu</h5>
-        <a class="ftcolordark" href="#">imanfajarlombu@gmail.com</p></a>
+      <div class="col pos-right">
+        <p>Aplikasi Rekomendasi Perpustakaan Digital Menggunakan Deep Collaborative Filtering Berbasis Mobile Dan Web</p>
+        <div class="flex">
+          <a class="footer-link" href="https://www.facebook.com"><i class="fab fa-facebook fa-2x"></i></a>
+          <a class="footer-link" href="https://www.instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
+          <a class="footer-link" href="https://www.twitter.com"><i class="fab fa-twitter fa-2x"></i></a>
+          <a class="footer-link" href="https://www.youtube.com"><i class="fab fa-youtube fa-2x"></i></a>
+        </div>
       </div>
-      <div class="col-md-4 col-sm-6 col-12">
-        <h5 class="warna">Martua Sinaga</h5>
-        <a class="ftcolordark" href="#">martuansinaga@gmail.com</p></a>
-      </div>
+    </div>
+    <div class="row mt-3 pos-center">
+      <div class="col">Copyright &#169; 2021 Perpustakaan Digital | Zoker</div>
     </div>
   </div>
 </footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
