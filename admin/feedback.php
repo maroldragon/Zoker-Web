555268870e8f7ca4185ed8f33aa4581f779f5e4c
maroldragon <autobot2690@gmail.com>
2021-07-20 01:06:20 +0700

    update feedback

--- admin/feedback.php
+++ admin/feedback.php
@@ -37,8 +37,8 @@
 						<div class="card-header">
 							<div class="mb-0">
 								<nav class="navbar ">
-									<form method="get" action="kelola-member-verifikasi.php">
-										<button class="btn btn-secondary" type="submit">
+									<form method="get" action="feedback.php?feedback">
+										<button id="print" class="btn btn-secondary" type="submit">
 											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
 												<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
 												<path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z" />
@@ -63,7 +63,7 @@
 						<div class="col-12">
 							<div class="card flex-fill">
 								<div class="card-header">
-									<table class="table table-hover my-0">
+									<table id="printTable" class="table table-hover my-0">
 										<thead id="data-table-feed" class="data-table-head">
 										
 										</thead>
@@ -119,6 +119,20 @@
 	<script src="js/custom.js"></script>
 	<script src="js/custom-user.js"></script>
 	<script>
+		function printData()
+			{
+			var divToPrint=document.getElementById("printTable");
+			newWin= window.open("");
+			newWin.document.write(divToPrint.outerHTML);
+			newWin.print();
+			newWin.close(location.href = "feedback.php?feedback");
+			
+			}
+
+			$('#print').on('click',function(){
+			printData();
+			})
+		//=============================================================//
 		SelectAllData()
 		function SelectAllData(){
 			
