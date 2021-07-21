3e6b954c745096293c939cebb1d45933bfa29776
Iman Fajar Lombu <vhajar18@gmail.com>
2021-07-21 17:52:59 +0700

    footer

--- index.php
+++ index.php
@@ -78,20 +78,10 @@
         </div>
       </div>
 
-      <div class="d-none col-12">
-        <nav aria-label="Page navigation">
-          <ul class="pagination justify-content-center">
-            <li class="page-item disabled">
-              <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-              <a class="page-link" href="#">Next</a>
-            </li>
-          </ul>
-        </nav>
+      <div class="row">
+        <div class="col">
+          <a class="btn btn-secondary form-control mt-" id="btnLebihBanyak">Tampilkan Lebih Banyak</a>
+        </div>
       </div>
     </div>
   </div>
